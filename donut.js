f459e9069468efc92330f7197279e04a1067ff87
Enenche23 <christopherejehelias158353@gmail.com>
2025-03-15 17:21:29 +0100

    I seperated the Js & Css From the html

---
+++ donut.js
@@ -0,0 +1,34 @@
+function ParseDonuts(donutString) {
+    numDonuts = parseInt(donutString);
+    if (donutString.indexOf("dozen") != -1) 
+        numDonuts *= 12;
+    return numDonuts; 
+}
+function updateOrder() {
+    const TAXRATE = 0.0925;
+    const DONUTPRICE = 0.50;
+    var numCakeDonuts = ParseDonuts(document.getElementById("cakedonuts").value);
+    var numGlazeDonuts = ParseDonuts(document.getElementById("glazedonuts").value); 
+    if (isNaN(numCakeDonuts))
+        numCakeDonuts = 0; 
+    if (isNaN(numGlazeDonuts))
+        numGlazeDonuts = 0; 
+    var subTotal = (numCakeDonuts + numGlazeDonuts) * DONUTPRICE;
+    var tax = subTotal * TAXRATE;
+    var total = subTotal + tax;
+    document.getElementById("subtotal").value = "$" + subTotal.toFixed(2);
+    document.getElementById("tax").value = "$" + tax.toFixed(2);
+    document.getElementById("total").value = "$" + total.toFixed(2);
+}
+
+
+function placeOrder() {
+    if(document.getElementById("name").value == "")
+        alert("I'm Sorry, but you must provide your name before submitting an order.");
+    else if(document.getElementById("time").value == "" || isNaN(document.getElementById("time").value))
+        alert (" I'm Sorry, but you must provide the number of minutes until pick-up before submitting an order.");
+
+    
+    // submit order to sender ...
+    form.submit();
+}
